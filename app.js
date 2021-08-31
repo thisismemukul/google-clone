113afed58e645d0be535cc4afd2afa5d3a14a1b5
Mukul Saini <thisismemukul@gmail.com>
2021-08-31 21:59:12 +0530

    brfore integrating serpaapi:

--- app.js
+++ app.js
@@ -24,14 +24,45 @@ app.get("/", function(req, res) {
 
 });
 
+// app.get("/search", function(req, res) {
+//     var api_key = "cbbde2093c7159a4cc2c9e483eccf03e";
+//     var query = "google";
+//     request("http://api.serpstack.com/search?access_key=" + api_key + "&query=" + query, function(error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//             const data = JSON.parse(body);
+//             var check_req = data["request"]["success"]
+//             if (check_req === true) {
+//                 res.render("home", { data: data });
+//             } else {
+//                 console.log("Server Error")
+//             }
+//         }
+//     });
+// });
+
+// app.get("/search", function(req, res) {
+//     var api_key = "2BC477F58495465FBCB97043A36843A7";
+//     var query = req.query.search;
+//     request("https://api.scaleserp.com/search?api_key=" + api_key + "&q=" + query + "&google_domain=google.co.in&location=Maharashtra,India&gl=in&hl=en", function(error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//             const data = JSON.parse(body);
+//             var check_req = data["request_info"]["success"]
+//             if (check_req === true) {
+//                 res.render("search", { data: data, query: query });
+//             } else {
+//                 console.log("Server Error")
+//             }
+//         }
+//     });
+// });
 
 app.get("/search", function(req, res) {
-    var api_key = "cbbde2093c7159a4cc2c9e483eccf03e";
+    var api_key = "2BC477F58495465FBCB97043A36843A7";
     var query = req.query.search;
-    request("http://api.serpstack.com/search?access_key=" + api_key + "&type=web&query=" + query, function(error, response, body) {
+    request("https://api.scaleserp.com/search?api_key=" + api_key + "&q=" + query + "&google_domain=google.co.in&location=Maharashtra,India&gl=in&hl=en", function(error, response, body) {
         if (!error && response.statusCode == 200) {
             const data = JSON.parse(body);
-            var check_req = data["request"]["success"]
+            var check_req = data["request_info"]["success"]
             if (check_req === true) {
                 res.render("search", { data: data, query: query });
             } else {
@@ -39,7 +70,6 @@ app.get("/search", function(req, res) {
             }
         }
     });
-    // res.render("search");
 });
 
 app.get("*", function(req, res) {
